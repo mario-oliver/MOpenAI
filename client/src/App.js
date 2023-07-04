6c6f995535f92b6fc8067211c9eb1962f0f87eb6
Mario Oliver <mar.mariooliver93@gmail.com>
2023-07-04 12:14:12 -0400

    ported chat to be dynamic based on state variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
 import './normalize.css';
 import chatgptlogo from './ChatGPT.svg';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [chatLog, setChatLog] = useState([
+    {
+      user: 'gpt',
+      message: 'How can I help you today',
+    },
+    {
+      user: 'me',
+      message: 'I want to use Chat GPT!',
+    },
+  ]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setChatLog([...chatLog, { user: 'me', message: input }]);
+    setInput('');
+  };
+
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -13,27 +31,37 @@ function App() {
 
       <section className="chatbox">
         <div className="chat-log">
-          <div className="chat-message">
-            <div className="chat-message-center">
-              <div className="avatar"></div>
-              <div className="message">I have a prompt for you, chatGPT</div>
-            </div>
-          </div>
-          <div className="chat-message chatgpt">
-            <div className="chat-message-center">
-              <div className="avatar chatgpt">
-                <img className="avatar_img" src={chatgptlogo} alt="chatgpt" />
+          {chatLog.map((chat, index) => {
+            return (
+              <div
+                className={`chat-message ${chat.user === 'gpt' && 'chatgpt'}`}
+              >
+                <div className="chat-message-center">
+                  <div className={`avatar ${chat.user === 'gpt' && 'chatgpt'}`}>
+                    {chat.user === 'gpt' && (
+                      <img
+                        className="avatar_img"
+                        src={chatgptlogo}
+                        alt="chatgpt"
+                      />
+                    )}
+                  </div>
+                  <div className="message">{chat.message}</div>
+                </div>
               </div>
-              <div className="message">I am an AI </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
         <div className="chat-input-holder">
-          <textarea
-            className="chat-input-textarea"
-            rows="1"
-            placeholder="Type your message here"
-          ></textarea>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="chat-input-textarea"
+              rows="1"
+              placeholder="Type your message here"
+            ></input>
+          </form>
         </div>
       </section>
     </div>
